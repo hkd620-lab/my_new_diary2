1cf70a1029c9797972cf5e0cf722b0464a112a84
경대 허 <hkd620@gmail.com>
2026-02-13 09:21:19 +0900

    chore: remove build artifacts and logs from git tracking

--- build/pages/Essay.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = Essay;
-function Essay() {
-    return (<div style={{ padding: 16 }}>
-      <h2>에세이 초고</h2>
-      <p>기록을 바탕으로 에세이를 준비합니다.</p>
-    </div>);
-}
